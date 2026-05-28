cfc5a9ed144582c66dc5364ef948faee933782ac
bianhaowen <bhaowen@qq.com>
2026-05-28 19:47:38 +0800

    1.4： （bug）频闪和黑色背景

--- app/src/main/java/com/vivo/shortcuts/MainActivity.kt
+++ app/src/main/java/com/vivo/shortcuts/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -46,7 +45,6 @@ import com.vivo.shortcuts.ui.theme.VivoShortcutsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             window.addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND)
